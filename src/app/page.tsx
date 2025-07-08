335e67a9a5de175e3ab6a342c0f1d8995ae9a436
zakariae2005 <lzakariae1937@gmail.com>
2025-07-08 13:28:59 +0200

    fix errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import AboutSection from "@/components/Landing/about-section";
+import FeaturedMenu from "@/components/Landing/featured-menu";
+import Footer from "@/components/Landing/footer";
+import LocationContact from "@/components/Landing/location-contact";
+import OpeningHours from "@/components/Landing/opening-hours";
+import RestaurantHero from "@/components/Landing/restaurant-hero";
+import Testimonials from "@/components/Landing/testimonials";
 
 
 export default function Home() {
